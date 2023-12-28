84866250aa2e598c5619e1930d205dc0090a27d6
Bantumoh <138443749+Bantumoh@users.noreply.github.com>
2023-12-28 11:43:56 +0200

    modified the search  form and the layout of the weather conditions. Also added a footer. on HTML. Removed the Fahrenheit / Celsius conversion.  On JS: Added a forecast url and a different weather icon url. On CSS: Changed the wether temp readings to red

--- src/index.js
+++ src/index.js
@@ -1,142 +1,112 @@
-let now = new Date();
-
-let today = document.querySelector(".date-time");
-
-let date = now.getDate();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let year = now.getFullYear();
-
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = days[now.getDay()];
-
-let months = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-let month = months[now.getMonth()];
-
-today.innerHTML = `Last updated: ${day} ${month} ${date} ${year}, ${hours}:${minutes}`;
-
-let apiKey = "44748b483f94fdf59996aa7a8c3e93ab";
-
-function updateWeatherData(city) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(function (response) {
-    let temperature = Math.round(response.data.main.temp);
-    let temperatureElement = document.querySelector("#temperature-placeholder");
-    temperatureElement.textContent = temperature;
-
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
-
-    let forecastData = [
-      { day: days[0], icon: "🌤️", high: 15, low: 9 },
-      { day: days[1], icon: "🌦️", high: 18, low: 10 },
-      { day: days[2], icon: "☁️", high: 20, low: 12 },
-      { day: days[3], icon: "⛈️", high: 16, low: 8 },
-      { day: days[4], icon: "🌧️", high: 14, low: 7 },
-    ];
-
-    displayForecast(forecastData);
-
-    let iconElement = document.querySelector("#icon");
-    let condition = document.querySelector("#condition");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind-speed");
-    iconElement.setAttribute(
-      "src",
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
-    );
-
-    windElement.innerHTML = `Wind speed: ${response.data.wind.speed} m/s`;
-    humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-    condition.innerHTML = response.data.weather[0].description;
-  });
-}
-
-function search(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#search-text-input").value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.textContent = cityInput;
-  updateWeatherData(cityInput);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
-let searchForm = document.querySelector("form");
-searchForm.addEventListener("submit", search);
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature-placeholder");
-  let currentTemperature = parseFloat(temperatureElement.textContent);
-
-  if (temperatureElement.dataset.unit === "celsius") {
-    let fahrenheitTemperature = (currentTemperature * 9) / 5 + 32;
-    temperatureElement.textContent = Math.round(fahrenheitTemperature);
-    temperatureElement.dataset.unit = "fahrenheit";
-    fahrenheitLink.classList.add("active");
-    celsiusLink.classList.remove("active");
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-function convertToCelsius(event) {
+function searchCity(city) {
+  let apiKey = "1467boea3fcafd3005c3ef2tba24a308";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature-placeholder");
-  let currentTemperature = parseFloat(temperatureElement.textContent);
+  let searchInput = document.querySelector("#search-form-input");
 
-  if (temperatureElement.dataset.unit === "fahrenheit") {
-    let celsiusTemperature = ((currentTemperature - 32) * 5) / 9;
-    temperatureElement.textContent = Math.round(celsiusTemperature);
-    temperatureElement.dataset.unit = "celsius";
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-  }
+  searchCity(searchInput.value);
 }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
 
-document.querySelector("#temperature-placeholder").dataset.unit = "celsius";
-celsiusLink.classList.add("active");
 
-function displayForecast(forecastData) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "1467boea3fcafd3005c3ef2tba24a308";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) { 
+  console.log(response.data);
+
+ 
   let forecastHtml = "";
 
-  forecastData.forEach(function (forecastDay) {
-    forecastHtml += `
-      <div class="col">
-        <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${forecastDay.day}</div>
-          <div class="weather-forecast-icon">${forecastDay.icon}</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5 ) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+            <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>${forecastDay.high}°</strong> | ${forecastDay.low}°
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}°</div>
         </div>
-      </div>`;
+      </div>
+    `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
+
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
+
+searchCity("Pretoria");
